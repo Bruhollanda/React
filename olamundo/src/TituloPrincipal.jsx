583b464d975f54eb7761d3d8f21f3b8f2f2da409
bruholanda <brunaholandacunha7@gmail.com>
2024-03-22 17:17:46 -0300

    utilização do evento onClick

--- olamundo/src/TituloPrincipal.jsx
+++ olamundo/src/TituloPrincipal.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function TituloPrincipal(props) {
   let titulo = props.titulo ?? "Olá, mundo!";
-  
+
+  function onClickHandler(event) {
+    alert("Clicou!");
+  }
+
   return(
-    <h1>{ titulo }</h1>
+    <div>
+      <h1>{ titulo }</h1>
+      <button onClick={onClickHandler}>Clique aqui!</button>
+    </div>
   );
 }
 
